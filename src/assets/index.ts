6fe251f36d032beb8bc2b8ce18044000d0190b28
Bruno Riwerson Silva <riwerson@gmail.com>
2025-11-24 08:11:44 -0300

    chore: create export file

---
+++ src/assets/index.ts
@@ -0,0 +1,65 @@
+import github from './images/contact/github.png';
+import linkedin from './images/contact/linkedin.png';
+
+import about from './images/navIcons/about.svg';
+import home from './images/navIcons/home.svg';
+import projects from './images/navIcons/projects.svg';
+import skills from './images/navIcons/skills.svg';
+
+import css from './images/skills/css.svg';
+import docker from './images/skills/docker.svg';
+import dotnet from './images/skills/dotnet.svg';
+import electron from './images/skills/electron.svg';
+import git from './images/skills/git.svg';
+import golang from './images/skills/golang.png';
+import html from './images/skills/html.svg';
+import java from './images/skills/java.svg';
+import javascript from './images/skills/javascript.svg';
+import jest from './images/skills/jest.png';
+import linux from './images/skills/linux.svg';
+import mongodb from './images/skills/mongodb.png';
+import mysql from './images/skills/mysql.png';
+import nodejs from './images/skills/nodejs.png';
+import postgresql from './images/skills/postgresql.svg';
+import python from './images/skills/python.png';
+import react from './images/skills/react.svg';
+import redux from './images/skills/redux.svg';
+import testingLibrary from './images/skills/testing-library.png';
+import typescript from './images/skills/typescript.svg';
+import vuejs from './images/skills/vuejs.svg';
+
+export const contact = {
+  github,
+  linkedin,
+};
+
+export const navIcons = {
+  about,
+  home,
+  projects,
+  skills,
+};
+
+export const skillIcons = {
+  css,
+  docker,
+  dotnet,
+  electron,
+  git,
+  golang,
+  html,
+  java,
+  javascript,
+  jest,
+  linux,
+  mongodb,
+  mysql,
+  nodejs,
+  postgresql,
+  python,
+  react,
+  redux,
+  testingLibrary,
+  typescript,
+  vuejs,
+};
