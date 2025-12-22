2e6c5a7c0fffd54dfe2be6e9ae7c1b8f055c57eb
Bruno Riwerson Silva <riwerson@gmail.com>
2025-12-22 15:59:33 -0300

    feat: create type for carousel icons

---
+++ src/interfaces/CarouselIcon.ts
@@ -0,0 +1,7 @@
+export interface CarouselIcon {
+  id: number;
+  src: string;
+  alt: string;
+  summary: string;
+  doc: string;
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,5 @@
 export type { Language } from './Language';
 export type { Project, ProjectCardTranslation } from './Project';
+export type { CarouselIcon } from './CarouselIcon';
 export type { Translation } from './Translation';
 export type { TranslationContextType } from './TranslationContextType';
